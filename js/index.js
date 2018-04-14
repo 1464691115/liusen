d44b784b8c615c82646abfabd8435ffbaf73e772
1464691115 <1464691115@qq.com>
2018-04-14 18:24:21 +0800

    分页的按钮特效

--- js/index.js
+++ js/index.js
@@ -26,6 +26,33 @@
             return target;
         }
         document.ready = function(){
+            //计算轮播图分页按钮
+            var paging = document.getElementById("pagingid"),
+                paging_li = paging.getElementsByTagName("li"),
+                paging_m= paging_li.childNodes;
+            console.log(paging_m);
+            // var left_paging = paging.getElementsByTagName("li")[1].currentStyle.innerWidth;
+            // console.log(paging.getElementsByTagName("li")[1].lenght);
+            // pading.style.left=left_paging;
+            for(var i=0;i<paging_li.length;i++){
+                // var m=null,f=null;
+                // if(i===0){
+                //     m = "m_cal",
+                //     f = "f_calh";
+                // }
+                // paging_li[i].innerHTML="<div id='f_id' class='f_cal "+f+"'></div><div class='cal "+m+"' id='m_id'></div>";
+                paging_li[i].innerHTML="<div id='f_id' class='f_cal'></div><div class='cal' id='m_id'></div>";
+                paging_li[i].onmousemove=function () {
+                    this.childNodes[1].className="m_cal";/*border-color: transparent;*/
+                    this.childNodes[0].className="f_cal f_calh";
+                    // paging_li[i].getElementsByClassName("cal")[0].classList.add("m_cal");
+                }
+                paging_li[i].onmouseout=function () {
+                    // paging_li[i].getElementsByClassName("cal")[0].classList.remove("m_cal");
+                    this.childNodes[1].className="cal";
+                    this.childNodes[0].className="f_cal";
+                }
+            }
             //事件委托
             // var ul = document.getElementById("navid");
             // ul.onclick=function(e){
