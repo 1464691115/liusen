69152971a61da23cf0c27d9f7c9d6753043798bc
1464691115 <1464691115@qq.com>
2018-04-30 19:26:14 +0800

    轮播图完善，下一布用户

--- js/jq_index.js
+++ js/jq_index.js
@@ -37,36 +37,55 @@
         $("#pagingid>li:first-child").children("div").first().addClass("f_calh");
         $("#pagingid>li:first-child").children("div").last().addClass("m_cal");
         //圆点切换||轮播图按键轮播
-        var zx_pagin = function (e) {
-            $("#pagingid>li").children("#f_id").removeClass("f_calh");
-            $("#pagingid>li").children("#m_id").removeClass("m_cal");
-            $(e).children("#m_id").addClass("m_cal");
-            $(e).children("#f_id").addClass("f_calh");
-            console.log();
+        var padingid=$("#pagingid>li"),
+        zx_pagin = function (e) {
+            padingid.children("#f_id").removeClass("f_calh");
+            padingid.children("#m_id").removeClass("m_cal");
+            e.children("#m_id").addClass("m_cal");
+            e.children("#f_id").addClass("f_calh");
+            return e.index();
         }
         $("#pagingid>li").hover(function(){
-            zx_pagin(this);
+            var paginidex = zx_pagin($(this));
+            $(".carouselUl>li").css({
+                "z-index" : 0,
+                "opacity" : 0
+            });
+            $(".carouselUl>li").eq(paginidex).css({
+                "z-index" : 1,
+                "opacity" : 1
+            });
         });
         //定时图片切换
-        $(".carouselUl>li").hide();
-        $(".carouselUl>li").first().show();
-        var time_Sum = 1,
+        // $(".carouselUl>li").first().css('z-index',1);
+        console.log($(".carouselUl>li").eq(0).css('z-index'));
+        var cale = $(".carouselUl>li").length,
         time_imgFun = function () {
-            for(var i=0;i<$(".carouselUl>li").length;i++){
-                $(".carouselUl>li").eq(i).fadeOut("fast",function () {
-                    if(i==5){
-                        $(".carouselUl>li").eq(time_Sum).fadeIn("fast",function () {
-                            if(time_Sum===5){
-                                time_Sum = 0;
-                            }
-                            zx_pagin($("#pagingid>li").eq(time_Sum-1));
+            for(var i=0;i<cale;i++){
+                if($(".carouselUl>li").eq(i).css('z-index')==1){
+                    zx_pagin(padingid.eq(i+1));
+                    $(".carouselUl>li").eq(i).css({
+                        "z-index" : 0,
+                        "opacity" : 0
+                    });
+                    if(i==cale-1){
+                        $(".carouselUl>li").eq(0).css({
+                            "z-index" : 1,
+                            "opacity" : 1
                         });
-                        time_Sum++;
+                        zx_pagin(padingid.eq(0));
+                        break;
+                    }else{
+                        $(".carouselUl>li").eq(i+1).css({
+                            "z-index" : 1,
+                            "opacity" : 1
+                        });
+                        break;
                     }
+                }
 
-                });
             }
-        },
+        }
         time_image = setInterval(time_imgFun,2500);
     });
 })(jQuery);
